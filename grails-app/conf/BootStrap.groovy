60c015da2ce2cfd4edf6696cf9da250f9036be9f
Michael Minella <mminella@criticalmass.com>
2012-01-31 17:06:17 -0600

    Refactored to support methods as well as closures

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -12,30 +12,58 @@ class BootStrap {
 		
 		grailsApplication.controllerClasses.each{ controller ->
 			def annotation = controller.getClazz().getAnnotation(Feature)
-			def isControllerFeature = annotation != null
 
-			controller.reference.propertyDescriptors.each{ pd -> 
-				def closure = controller.getPropertyOrStaticPropertyOrFieldValue(pd.name, Closure)
+			parseActionClosures(controller, annotation, controlledActions)
+			parseActionMethods(controller, annotation, controlledActions)
+		}
+	
+		servletContext["controlledActions"] = controlledActions
+	}
+	
+	def parseActionClosures = { controller, annotation, controlledActions ->
+		def isControllerFeature = annotation != null
+		
+		controller.reference.propertyDescriptors.each{ pd -> 
+			def closure = controller.getPropertyOrStaticPropertyOrFieldValue(pd.name, Closure)
+		
+			if(closure) {
+				if(pd.name != 'beforeInterceptor' && pd.name != 'afterInterceptor') {
+					Field curField = controller.getClazz().getDeclaredField(pd.name)
+					def key = controller.name.toLowerCase() + '.' + pd.name.toLowerCase()
 				
-				if(closure) {
-					if(pd.name != 'beforeInterceptor' && pd.name != 'afterInterceptor') {
-						Field curField = controller.getClazz().getDeclaredField(pd.name)
-						
-						def fieldAnnotation = curField.getAnnotation(Feature)
-						
-						
-						if(fieldAnnotation != null) {
-							Result result = new Result(name:fieldAnnotation.name(), resultStatus:fieldAnnotation.responseStatus(), resultRedirect:fieldAnnotation.responseRedirect())
-							controlledActions.put(controller.name.toLowerCase() + '.' + pd.name.toLowerCase(), result)
-						} else if(isControllerFeature) {
-							Result result = new Result(name:annotation.name(), resultStatus:annotation.responseStatus(), resultRedirect:annotation.responseRedirect())
-							controlledActions.put(controller.name.toLowerCase() + '.' + pd.name.toLowerCase(), result)
-						}
+					def fieldAnnotation = curField.getAnnotation Feature
+				
+					if(fieldAnnotation != null) {
+						Result result = new Result(name:fieldAnnotation.name(), resultStatus:fieldAnnotation.responseStatus(), resultRedirect:fieldAnnotation.responseRedirect())
+						controlledActions.put(key, result)
+					} else if(isControllerFeature) {
+						Result result = new Result(name:annotation.name(), resultStatus:annotation.responseStatus(), resultRedirect:annotation.responseRedirect())
+						controlledActions.put(key, result)
 					}
 				}
 			}
 		}
-	
-		servletContext["controlledActions"] = controlledActions
+	}
+
+	def parseActionMethods = { controller, annotation, controlledActions ->
+		def isControllerFeature = annotation != null
+
+		controller.clazz.declaredMethods.each{ method ->
+		
+			if(method.name.indexOf('$') < 0) {
+				def methodAnnotation = method.getAnnotation Feature
+				def key = controller.name.toLowerCase() + '.' + method.name.toLowerCase()
+
+				if(controlledActions[key] == null) {
+					if(methodAnnotation != null) {
+						Result result = new Result(name:methodAnnotation.name(), resultStatus:methodAnnotation.responseStatus(), resultRedirect:methodAnnotation.responseRedirect())
+						controlledActions.put(key, result)
+					} else if(isControllerFeature) {
+						Result result = new Result(name:annotation.name(), resultStatus:annotation.responseStatus(), resultRedirect:annotation.responseRedirect())
+						controlledActions.put(key, result)
+					}
+				}
+			}
+		}
 	}
 }
