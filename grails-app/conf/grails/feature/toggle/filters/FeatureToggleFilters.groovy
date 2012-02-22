6d9ea17e81e8f0b2da90d682ccf090fb94523155
Ryan Norris <ryannorris@gmail.com>
2012-02-22 16:51:34 -0500

    merged pull request

--- grails-app/conf/grails/feature/toggle/filters/FeatureToggleFilters.groovy
+++ grails-app/conf/grails/feature/toggle/filters/FeatureToggleFilters.groovy
@@ -1,22 +1,43 @@
 package grails.feature.toggle.filters
 import grails.plugin.featuretoggle.FeatureToggleService;
+import grails.plugin.featuretoggle.annotations.Feature;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-class FeatureToggleFilters {
+class FeatureToggleFilters implements ApplicationContextAware {
 
 	FeatureToggleService featureToggleService
-
+	ApplicationContext applicationContext
+	
 	def filters = {
 		allControllers(controller:'*', action:'*') {
 			before = { 
 				def controllers = request.servletContext['controlledActions']
 				
-				def action = controllers[controllerName.toLowerCase() + '.' + actionName.toLowerCase()] 
+				def curAction = actionName
+
+				// In the case of the action not being specified (default action) we need to look up 
+				// if it was configured in the controller or it is using the convention 'index'
+				if(curAction == null) {
+					def artefact = grailsApplication.getArtefactByLogicalPropertyName("Controller", controllerName)
+
+					def bean = applicationContext.getBean(artefact.clazz.name)
+					def metaProperty = bean.metaClass.getMetaProperty("defaultAction")
+
+					if(metaProperty) {
+						curAction = bean.defaultAction
+					} else {
+						curAction = 'index'
+					}
+				}
+				
+				def action = controllers[controllerName?.toLowerCase() + '.' + curAction?.toLowerCase()] 
 				
 				if(action != null && !featureToggleService.isFeatureEnabled(action.name)) {
 					if(action.resultRedirect.size() > 0) {
 						redirect(uri: action.resultRedirect)
 					} else {
-						render(status: action.resultStatus )
+						render(status: action.resultStatus)
 					}
 				}
 			}
